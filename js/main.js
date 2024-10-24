fc0875345e314e5d5255a564e329c30849607c29
Adriverion <dantejose7@gmail.com>
2024-10-23 20:31:10 -0400

    corrections

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@ import Input from "./Input.js";
 const $inputBill = new Input(document.getElementById('input-bill'), 8);
 const $inputPercentage = new Input(document.getElementById('input-percentage'), 3);
 const $inputPeople = new Input(document.getElementById('input-people'), 3);
-const $defaultButton = document.querySelector('button[value="15"]');
+const $defaultButtons = document.querySelectorAll('button.calculator__form-panel-item');
+const $resetButton = document.getElementById('reset');
 const $resultTip = document.getElementById('tip-amount');
 const $result = document.getElementById('total');
 
 const check = {
     bill: 0,
-    percentage: 15,
+    percentage: 0,
     people: 0,
     tip() { return (this.bill * this.percentage) / 100 },
     totalPerson() { return (this.bill + this.tip()) / this.people },
     tipPerson() {return this.tip() / this.people}
 }
 
-const defaultPercentage = (elem = null, noSelect = true) => {
-    const $parent = (elem === null)? $defaultButton.parentElement : elem.parentElement;
-    for (let item of $parent.children) 
+const defaultPercentage = (elem = null) => {
+    for (let item of $defaultButtons) 
         item.classList.remove('calculator__button--cyan');
 
-    if (noSelect) {
+    if (elem != null) {
         elem.classList.add('calculator__button--cyan');
         check.percentage = parseInt(elem.value);
     }
@@ -31,11 +31,13 @@ const reset = () => {
     $inputBill.clear();
     $inputPercentage.clear();
     $inputPeople.clear();
-    defaultPercentage($defaultButton);
     $result.textContent = '$0.00';
     $resultTip.textContent = '$0.00';
+    defaultPercentage();
     check.bill = 0;
     check.people = 0;
+    check.percentage = 0;
+    $resetButton.disabled = true;
 }
 
 const showResult = () => {
@@ -43,13 +45,15 @@ const showResult = () => {
         $result.textContent = `$${check.totalPerson().toFixed(2)}`;
         $resultTip.textContent = `$${check.tipPerson().toFixed(2)}`;
     }
+
 }
 
 window.addEventListener('click', e => {
     if (e.target.matches('button.calculator__form-panel-item')) defaultPercentage(e.target);
-    if (e.target.matches('input.calculator__form-panel-item')) defaultPercentage(null, false);
-    if (e.target.value === "reset") reset();
+    if (e.target.matches('input.calculator__form-panel-item')) defaultPercentage();
+    $resetButton.disabled = false;
     showResult();
+    if (e.target === $resetButton) reset();
 })
 
 window.addEventListener('input', e => {
